5825aa55a351788a89fab2ea273b51fd5a295d55
Andrew Wang <andrewdotwang@gmail.com>
2024-02-28 21:59:57 -0800

    changes

--- nan_lab8/tsdb/skeletons/Index.lisp
+++ nan_lab8/tsdb/skeletons/Index.lisp
@@ -21,5 +21,5 @@
 
 (
 ((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
-((:path . "lab7") (:content . "Test suite collected for Lab 7."))
+((:path . "lab8") (:content . "Test suite collected for Lab 8."))
 )
